06bcbed67f76f2f7aabbd2092788702ef4feae06
phuong95st <phuongtran95st@gmail.com>
2016-06-28 16:46:44 +0700

    link commit bài tập

---
+++ 04_Output/04_BaiTapCuoiKhoa/Debug/src/manager_user/WebContent/js/user.js
@@ -0,0 +1,10 @@
+function getInputForm() {
+	var frm = document.inputform;
+	if(frm == undefined) frm = document.mainform;
+	return frm;
+}
+function paging(url, pageId) {
+	var frm = getInputForm();
+	frm.page.value = pageId;
+	frm.submit();
+}
